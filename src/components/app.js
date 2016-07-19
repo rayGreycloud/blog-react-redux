9a7b3007da11a4f538f5346a153c86637b8a4675
rayGreycloud <rayGreycloud@gmail.com>
2016-07-19 10:07:56 -0700

    Add routing and first view component

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      This is the App component
+      {this.props.children}
+      </div>
+
     );
   }
 }
